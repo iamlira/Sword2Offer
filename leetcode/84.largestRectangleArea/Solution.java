6d02b3e992d2d5d34863b12c33c1a81037e6c276
iamlira <474948760@qq.com>
2018-07-16 15:16:11 +0800

    add num 84

---
+++ leetcode/84.largestRectangleArea/Solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    /*
+    题目描述：https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+    思路：此题关键在于找到当前遍历矩形的左右边界，如果用传统做法肯定会超时，参考网上思路
+    对于一个升续的数组来说，例如：1,2,3,4,5，能得到最大的矩形面积一定是max(height[i]*(size-i))
+    这是因为对于每个矩形来说他的右边界已知，肯定是数组的末尾，左边界也已知，就是他自己，
+    所以对于不规则的数组，需要构造出一个升续的数组，而且在构造过程中能够求出左边界，并求出面积
+    这里使用栈进行实现，当前遍历的矩阵若大于等于栈顶，则符合升续数组，入栈即可
+    若当前遍历的矩阵小于栈顶，说明不是升续的，又从另外一个角度说明当前索引是栈顶元素的右边界，
+    此时则弹出栈顶元素，并计算面积：栈顶元素×（当前索引-栈顶元素索引）
+    如果当前矩阵一直小于栈顶元素，则重复上述操作，计算结束后把原来的位置用当前值填补！意味着之前的位置当前矩阵都可以拓展到！
+    在升续的栈中，左边界是他自己，右边界是遍历到需要进行出栈的矩阵索引，所以可以得到所有矩阵在数组中的面积
+     */
+    public int largestRectangleArea(int[] heights) {
+        int result=0,count=0;
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<heights.length;i++){
+            count=0;//是用count来计算弹出元素数量多少
+            if(stack.isEmpty()||heights[i]>=stack.peek()){//如果栈空或者比栈顶元素大则入栈
+                stack.push(heights[i]);
+            }else if(heights[i]<stack.peek()){
+                while(!stack.isEmpty()&&heights[i]<stack.peek()){
+                    count++;
+                    int tmp=stack.peek();
+                    result=tmp*(i-stack.size()+1)>result?tmp*(i-stack.size()+1):result;//计算面积：栈顶元素×（当前索引-栈顶元素索引）
+                    stack.pop();
+                }
+                while(count>0){
+                    stack.push(heights[i]);
+                    count--;
+                }
+                stack.push(heights[i]);
+            }
+        }
+        for(int i=stack.size(),size=stack.size();i>0;i--){//计算升续数组的最大值
+            result=stack.peek()*(size-i+1)>result?stack.peek()*(size-i+1):result;
+            stack.pop();
+        }
+        return result;
+    }
+}
